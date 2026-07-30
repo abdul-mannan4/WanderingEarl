7e29bb0233aa6aabc5eedcae57cfde0d8662e5a7
Abdul Mannan <mannanibrahim321@gmail.com>
2026-07-30 12:17:39 +0500

    Comment  Component issue resolved

--- components/LandingPage/ClientReview/review.ts
+++ components/LandingPage/ClientReview/review.ts
@@ -1,33 +1,18 @@
-export const reviews=[
+export const reviews = [
     {
-        description:"Thank you for such a well thought out and memorable trip. The sights and people of Morocco are wonderful. I appreciated you feeding my nerdy self with information on the geography, animals and plants of Morocco. The meals were laid back and wonderful and I appreciated the pace. Truly memorable. Thank you for your hard work, phenomenal organization and attention to the needs of the group. ",
-        name:"Kathy J.",
-        location:"Morocco"
+        description: "Thank you for such a well thought out and memorable trip. The sights and people of Morocco are wonderful. I appreciated you feeding my nerdy self with information on the geography, animals and plants of Morocco. The meals were laid back and wonderful and I appreciated the pace. Truly memorable. Thank you for your hard work, phenomenal organization and attention to the needs of the group. ",
+        name: "Kathy J.",
+        location: "Morocco"
     },
-        {
-        description:"Wandering through Romania was an incredible trip in every way: great sights, great people, and everything was very well-organized. This was an excellent trip in which we got to visit many beautiful places (both well-known and off the beaten path) all while in the company of a small, cohesive group of like-minded travelers. I’ve traveled to 40+ countries and this trip still stands out in my mind as one of the best I’ve been on. I would highly recommend it! ",
-        name:"Nadia A.",
-        location:"Romania"
+    {
+        description: "Wandering through Romania was an incredible trip in every way: great sights, great people, and everything was very well-organized. This was an excellent trip in which we got to visit many beautiful places (both well-known and off the beaten path) all while in the company of a small, cohesive group of like-minded travelers. I’ve traveled to 40+ countries and this trip still stands out in my mind as one of the best I’ve been on. I would highly recommend it! ",
+        name: "Nadia A.",
+        location: "Romania"
     },
-            {
-        description:"The trip was amazing, on every level. Your experience and curiosity has created an amazing itinerary, and your willingness to accommodate the random wishes that came up was above and beyond. India is just endless fascination. The accommodations were great, and as someone that doesn’t really care about the food experience…it was great!!",
-        name:"Tricia S.",
-        location:"Morocco"
+    {
+        description: "The trip was amazing, on every level. Your experience and curiosity has created an amazing itinerary, and your willingness to accommodate the random wishes that came up was above and beyond. India is just endless fascination. The accommodations were great, and as someone that doesn’t really care about the food experience…it was great!!",
+        name: "Tricia S.",
+        location: "Morocco"
     },
-                {
-        description:"The trip was amazing, on every level. Your experience and curiosity has created an amazing itinerary, and your willingness to accommodate the random wishes that came up was above and beyond. India is just endless fascination. The accommodations were great, and as someone that doesn’t really care about the food experience…it was great!!",
-        name:"Tricia S.",
-        location:"Morocco"
-    },
-                {
-        description:"The trip was amazing, on every level. Your experience and curiosity has created an amazing itinerary, and your willingness to accommodate the random wishes that came up was above and beyond. India is just endless fascination. The accommodations were great, and as someone that doesn’t really care about the food experience…it was great!!",
-        name:"Tricia S.",
-        location:"Morocco"
-    },
-                    {
-        description:"The trip was amazing, on every level. Your experience and curiosity has created an amazing itinerary, and your willingness to accommodate the random wishes that came up was above and beyond. India is just endless fascination. The accommodations were great, and as someone that doesn’t really care about the food experience…it was great!!",
-        name:"Tricia S.",
-        location:"Morocco"
-    }
-    
+
 ]

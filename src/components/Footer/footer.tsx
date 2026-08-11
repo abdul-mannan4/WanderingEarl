a2e46f7a683961336c0ed82bc2d1063472f2b0fc
Abdul Mannan <mannanibrahim321@gmail.com>
2026-08-11 13:49:23 +0500

    Layout Inconsistency and Map Setup

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -32,7 +32,7 @@ export default function Footer() {
       />
       <div className="bg-[linear-gradient(0deg,rgba(30,54,92,0)_0%,rgba(30,54,92,0.95)_54.63%,#1E365C_75.11%,#1E365C_100%)] absolute inset-0" />
 
-      <div className="container flex flex-col justify-between h-full gap-[24px] sm:gap-[32px] md:gap-[50px] 2xl:gap-[106px] relative z-10">
+      <div className="container flex flex-col justify-between h-full gap-[24px] sm:gap-[32px] md:gap-[50px] 2xl:gap-[106px] relative 2xl:!px-[80px] z-10">
         <div className="2xl:pt-[80px] flex flex-col md:flex-row gap-6 sm:gap-8 md:gap-[24px] lg:gap-[60px] xl:gap-[100px] 2xl:gap-[148px]">
           {/* Logo & Info Block - Centered on Mobile/Tablet */}
           <div className="flex flex-col items-center md:items-start text-center md:text-left gap-[12px] sm:gap-[18px] md:gap-[18px] lg:gap-[24px] 2xl:gap-[30px] w-full max-w-[520px] md:w-[220px] lg:w-[340px] xl:w-[380px] 2xl:w-[425px] shrink-0 mx-auto md:mx-0">
